032e9032b161bf5630028904eec59ea39d4a352f
Light <[ghost1928ps@icloud.com]>
2020-05-21 11:02:31 +0900

    変更

--- python/pythontest.py
+++ python/pythontest.py
@@ -14,36 +14,51 @@
 # else:
 #     print('高齢')
 
+#文字列検索する場合＝＝は使わずにinを使う true falseが返ってくる
 # name = '情報花子'
-# if name == '子':
+# if '子' in name:
 #     print('有')
 # else:
 #     print('無')
 
 # moji = 'P'
-# while moji <= 'PPPP':
+#inを使わなくてもmoji < 'pppp'とかでも動く
+# while moji in 'PPPP':  
 #     moji = 'P' + moji
 # print(moji)
 
 #別解
 # moji = 'P'
-# while moji < 'PPPPP':
+# while moji in 'PPPPP':
 #     moji = 'P' + moji
 # print(moji)
 
+
+#str(変数)　数値を文字列に変換
 # temp = 35.5
 # while temp <= 38.0:
-#     print('平熱です')
+#     print(str(temp) + 'は平熱です')
 #     temp += 0.5
 #     if temp == 37.0:
 #         break
-# print('微熱です')
+# このprintはwhileを抜けた後
+# print(str(temp) + 'は微熱です')
 
 # hoge = ['12','1234','123']
 # for h in hoge:
 #     if len(h) >= 4:
 #         continue
-#         print(h)
+# 実行されないため必要なし(print(h))
 #     while len(h) < 4:
-#         h = '0' + h #左からつめる
+#         h = '0' + h #文字列は左からつめる　数値は右からつめる
 #     print(h)
+
+# hoge = ['12','1234','123']
+# for h in hoge:
+#     if len(h) >= 4:
+#         break
+#     while len(h) < 4:
+#         h = '0' + h
+#     print(h)
+# else: for文でのelseは、for文が正常処理できた場合のみ処理される
+#     print('OK')
