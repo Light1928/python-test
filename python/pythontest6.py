8c70ed4cfe5fc2e01bd442a73e007e8fd2f12518
Light <[ghost1928ps@icloud.com]>
2020-06-03 11:02:35 +0900

    update

--- python/pythontest6.py
+++ python/pythontest6.py
@@ -22,3 +22,48 @@
 # moge = 20
 # poge = 30
 # print(hoge,moge,poge)
+
+# hoge = [10,20,30]
+# for h1,h2 in enumerate(hoge,1): #開始値と要素をそれぞれの変数に代入　デフォルトは０
+#     print(h1,h2)
+
+# def test(*x): #０以上の変数の個数　タプルになる
+#     print(x)
+# test(1,2)
+# test()
+
+# def sum(*number):
+#     s = 0
+#     for n in number:
+#         s += n
+#     print(s)
+
+# sum(1,2)
+
+# 集合
+# hoge = {10,20,30,20,10} #重複は１つにまとめられる
+# print(hoge)
+# print(hoge[0]) #添字の概念がない
+
+# hoge = [10,20,30,20,10]
+# print(hoge)
+# print(type(hoge)) #type　型が分かる
+
+# moge = set(hoge) #set 集合に変わる
+# print(moge) #また、集合(set)は重複を持たない
+# print(type(moge))
+
+# hoge = {10,20,30,20,10}
+# print(hoge)
+
+# print(30 in hoge) #trueが返る hogeに30が入っているか
+
+# print(20 not in hoge)#falseが返る
+
+# hoge = {10, 20, 30}
+# hoge |= {40, 50}  # バラバラに挿入される
+# print(hoge)
+
+# hoge = {10, 20, 30}
+# hoge -= {20}
+# print(20)
