203e12ee34b7ee34390d76dc71705cebfe663010
Light <[ghost1928ps@icloud.com]>
2020-06-11 03:52:18 +0900

    DBを参照できるように設定

--- python_db_test/todo_list/models.py
+++ python_db_test/todo_list/models.py
@@ -1,8 +1,269 @@
 from django.db import models
-
 from django.utils import timezone
-#データベース名は既に作成済み
-#class テーブル名
+
+#python manage.py inspectdb >> /models.pyを置く場所/models.py  
+#これで自動で既存テーブルを使えるようになる
+#models.pyからテーブルを作ることは可能だが、バックエンドのことを考えると直接SQLで設定した方がいい？
+# #class テーブル名　テーブル名は実際のテーブル名と違っていても良い　class Metaの部分で指定可能
+class User(models.Model):
+    id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    name = models.CharField(db_column='NAME', max_length=10, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'USER'
+
+
+# class AuthGroup(models.Model):
+#     name = models.CharField(unique=True, max_length=150)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_group'
+
+
+# class AuthGroupPermissions(models.Model):
+#     group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+#     permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_group_permissions'
+#         unique_together = (('group', 'permission'),)
+
+
+# class AuthPermission(models.Model):
+#     name = models.CharField(max_length=255)
+#     content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+#     codename = models.CharField(max_length=100)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_permission'
+#         unique_together = (('content_type', 'codename'),)
+
+
+# class AuthUser(models.Model):
+#     password = models.CharField(max_length=128)
+#     last_login = models.DateTimeField(blank=True, null=True)
+#     is_superuser = models.IntegerField()
+#     username = models.CharField(unique=True, max_length=150)
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=150)
+#     email = models.CharField(max_length=254)
+#     is_staff = models.IntegerField()
+#     is_active = models.IntegerField()
+#     date_joined = models.DateTimeField()
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_user'
+
+
+# class AuthUserGroups(models.Model):
+#     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+#     group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_user_groups'
+#         unique_together = (('user', 'group'),)
+
+
+# class AuthUserUserPermissions(models.Model):
+#     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+#     permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_user_user_permissions'
+#         unique_together = (('user', 'permission'),)
+
+
+# class DjangoAdminLog(models.Model):
+#     action_time = models.DateTimeField()
+#     object_id = models.TextField(blank=True, null=True)
+#     object_repr = models.CharField(max_length=200)
+#     action_flag = models.PositiveSmallIntegerField()
+#     change_message = models.TextField()
+#     content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+#     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'django_admin_log'
+
+
+# class DjangoContentType(models.Model):
+#     app_label = models.CharField(max_length=100)
+#     model = models.CharField(max_length=100)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'django_content_type'
+#         unique_together = (('app_label', 'model'),)
+
+
+# class DjangoMigrations(models.Model):
+#     app = models.CharField(max_length=255)
+#     name = models.CharField(max_length=255)
+#     applied = models.DateTimeField()
+
+#     class Meta:
+#         managed = False
+#         db_table = 'django_migrations'
+
+
+# class DjangoSession(models.Model):
+#     session_key = models.CharField(primary_key=True, max_length=40)
+#     session_data = models.TextField()
+#     expire_date = models.DateTimeField()
+
+#     class Meta:
+#         managed = False
+#         db_table = 'django_session'
+
+
+# class TodoListInfomodelform(models.Model):
+#     name = models.CharField(max_length=255)
+#     mail = models.CharField(max_length=255)
+#     gender = models.IntegerField()
+#     department = models.CharField(max_length=255)
+#     year = models.IntegerField()
+#     created_at = models.DateField()
+
+#     class Meta:
+#         managed = False
+#         db_table = 'todo_list_infomodelform'
+# # This is an auto-generated Django model module.
+# # You'll have to do the following manually to clean this up:
+# #   * Rearrange models' order
+# #   * Make sure each model has one field with primary_key=True
+# #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+# #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# # Feel free to rename the models, but don't rename db_table values or field names.
+# from django.db import models
+
+
+# class User(models.Model):
+#     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
+#     name = models.CharField(db_column='NAME', max_length=10, blank=True, null=True)  # Field name made lowercase.
+
+#     class Meta:
+#         managed = False
+#         db_table = 'USER'
+
+
+# class AuthGroup(models.Model):
+#     name = models.CharField(unique=True, max_length=150)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_group'
+
+
+# class AuthGroupPermissions(models.Model):
+#     group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+#     permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_group_permissions'
+#         unique_together = (('group', 'permission'),)
+
+
+# class AuthPermission(models.Model):
+#     name = models.CharField(max_length=255)
+#     content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+#     codename = models.CharField(max_length=100)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_permission'
+#         unique_together = (('content_type', 'codename'),)
+
+
+# class AuthUser(models.Model):
+#     password = models.CharField(max_length=128)
+#     last_login = models.DateTimeField(blank=True, null=True)
+#     is_superuser = models.IntegerField()
+#     username = models.CharField(unique=True, max_length=150)
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=150)
+#     email = models.CharField(max_length=254)
+#     is_staff = models.IntegerField()
+#     is_active = models.IntegerField()
+#     date_joined = models.DateTimeField()
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_user'
+
+
+# class AuthUserGroups(models.Model):
+#     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+#     group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_user_groups'
+#         unique_together = (('user', 'group'),)
+
+
+# class AuthUserUserPermissions(models.Model):
+#     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+#     permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_user_user_permissions'
+#         unique_together = (('user', 'permission'),)
+
+
+# class DjangoAdminLog(models.Model):
+#     action_time = models.DateTimeField()
+#     object_id = models.TextField(blank=True, null=True)
+#     object_repr = models.CharField(max_length=200)
+#     action_flag = models.PositiveSmallIntegerField()
+#     change_message = models.TextField()
+#     content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+#     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'django_admin_log'
+
+
+# class DjangoContentType(models.Model):
+#     app_label = models.CharField(max_length=100)
+#     model = models.CharField(max_length=100)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'django_content_type'
+#         unique_together = (('app_label', 'model'),)
+
+
+# class DjangoMigrations(models.Model):
+#     app = models.CharField(max_length=255)
+#     name = models.CharField(max_length=255)
+#     applied = models.DateTimeField()
+
+#     class Meta:
+#         managed = False
+#         db_table = 'django_migrations'
+
+
+# class DjangoSession(models.Model):
+#     session_key = models.CharField(primary_key=True, max_length=40)
+#     session_data = models.TextField()
+#     expire_date = models.DateTimeField()
+
+#     class Meta:
+#         managed = False
+#         db_table = 'django_session'
+
+
 class InfoModelForm(models.Model):
     name = models.CharField('名前',max_length=255)
     mail = models.EmailField('メール',max_length=255)
--- python_db_test/todo_list/views.py
+++ python_db_test/todo_list/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from .forms import TestForm
-
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User  # 追加する
 def home(request):
     form = {
         'name':'Wood Village',
         'form':TestForm(),
         'insert_forms':'初期値',
+        'messages': User.objects.all(),
     }
     #カッコの記述違いに注意
     if(request.method == 'POST'):
         form['insert_forms'] = '文字列:' + request.POST['text'] + '<br>整数型:' + request.POST['num']
         form['form'] = TestForm(request.POST)
+    
 
     return render(request, 'home.html',form)
